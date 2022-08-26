47c3e8275f081a6d52ad804dff62c0578f9842fa
Excalizan <aydinkement24@gmail.com>
2022-08-26 16:48:01 +0300

    the cube got so f***ing better

--- cube/main.js
+++ cube/main.js
@@ -1,27 +1,35 @@
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-)
-
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight,
+	1,
+	10000
+)
+
+controls = new THREE.OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.25
+controls.enableZoom = true
+
+camera.position.z = 5
+controls.update()
+
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material = new THREE.MeshNormalMaterial()
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 5
-
 const animate = function () {
 	requestAnimationFrame(animate)
+	controls.update()
 
-	cube.rotation.x += 0.01
-	cube.rotation.y += 0.01
+	cube.rotation.x += 0.005
+	cube.rotation.y += 0.005
 
 	renderer.render(scene, camera)
 }
